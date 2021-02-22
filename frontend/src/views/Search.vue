957593371ba8de4dacc8146f3b458bf1cde21977
snomura0128 <tomra.3104@gmail.com>
2021-02-23 00:24:53 +0900

    add ward checkbox

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -1,10 +1,14 @@
 <template>
   <v-container class="spacing-playground pa-6" fluid>
     <v-row>
-      <v-col v-for="card in cards" :key="card.title" :cols="card.flex">
+      <v-col>
         <v-card>
-          <v-img :src="card.src" class="white--text align-end" height="200px">
-            <v-card-title v-text="card.title"></v-card-title>
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
+            class="white--text align-end"
+            height="200px"
+          >
+            <v-card-title></v-card-title>
           </v-img>
 
           <v-card-actions>
@@ -26,6 +30,32 @@
       </v-col>
     </v-row>
     <v-row>
+      <v-col
+        v-for="(wardpart, i) in [
+          wards.slice(0, 4),
+          wards.slice(4, 8),
+          wards.slice(8, 12),
+          wards.slice(12, 16),
+          wards.slice(16, 20),
+          wards.slice(20, 23),
+        ]"
+        v-bind:key="i"
+        cols="12"
+        sm="2"
+        md="2"
+      >
+        <v-checkbox
+          v-for="ward in wardpart"
+          v-bind:key="ward.value"
+          v-bind:label="ward.text"
+          v-bind:id="ward.key"
+          hide-details
+          value="true"
+        ></v-checkbox>
+      </v-col>
+    </v-row>
+
+    <v-row>
       <v-col class="d-flex align-center" cols="12" sm="6">
         <v-text-field>
           <v-icon slot="append" color="red"> mdi-plus </v-icon>
@@ -33,7 +63,7 @@
         </v-text-field>
       </v-col>
       <v-col class="d-flex align-center" cols="12" sm="6">
-        <v-select :items="items" label="Outlined style" outlined></v-select>
+        <v-select label="Outlined style" outlined></v-select>
       </v-col>
     </v-row>
   </v-container>
@@ -42,28 +72,37 @@
 <script>
 export default {
   data: () => ({
-    cards: [
-      {
-        title: "Pre-fab homes",
-        src: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
-        flex: 12,
-      },
-      {
-        title: "Favorite road trips",
-        src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
-        flex: 6,
-      },
-      {
-        title: "Best airlines",
-        src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
-        flex: 6,
-      },
-    ],
-    items: [
-      { icon: "mdi-contacts", text: "search", path: "/search" },
-      { icon: "mdi-history", text: "Frequently contacted", path: "/aaa" },
-      { icon: "mdi-content-copy", text: "Duplicates", path: "/ccc" },
+    wards: [
+      { value: 1, text: "足立区" },
+      { value: 2, text: "荒川区" },
+      { value: 3, text: "板橋区" },
+      { value: 4, text: "江戸川区" },
+      { value: 5, text: "大田区" },
+      { value: 6, text: "葛飾区" },
+      { value: 7, text: "北区" },
+      { value: 8, text: "江東区" },
+      { value: 9, text: "品川区" },
+      { value: 10, text: "渋谷区" },
+      { value: 11, text: "新宿区" },
+      { value: 12, text: "杉並区" },
+      { value: 13, text: "墨田区" },
+      { value: 14, text: "世田谷区" },
+      { value: 15, text: "台東区" },
+      { value: 16, text: "千代田区" },
+      { value: 17, text: "中央区" },
+      { value: 18, text: "豊島区" },
+      { value: 19, text: "中野区" },
+      { value: 20, text: "練馬区" },
+      { value: 21, text: "文京区" },
+      { value: 22, text: "港区" },
+      { value: 23, text: "目黒区" },
     ],
   }),
 };
 </script>
+
+<style scoped>
+.v-input--selection-controls {
+  margin-top: 0;
+}
+</style>
