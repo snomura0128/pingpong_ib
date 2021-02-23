f83b39ababd84ac72e0f6008532dabb802f95e2a
snomura0128 <tomra.3104@gmail.com>
2021-02-23 20:27:16 +0900

    add search card

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -1,108 +1,156 @@
 <template>
-  <v-container class="spacing-playground pa-6" fluid>
+  <v-container class="spacing-playground pa-2" fluid>
     <v-row>
       <v-col>
-        <v-card>
-          <v-img
-            src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
-            class="white--text align-end"
-            height="200px"
-          >
-            <v-card-title></v-card-title>
-          </v-img>
-
-          <v-card-actions>
-            <v-spacer></v-spacer>
-
-            <v-btn icon>
-              <v-icon>mdi-heart</v-icon>
-            </v-btn>
-
-            <v-btn icon>
-              <v-icon>mdi-bookmark</v-icon>
-            </v-btn>
-
-            <v-btn icon>
-              <v-icon>mdi-share-variant</v-icon>
-            </v-btn>
-          </v-card-actions>
+        <v-card class="spacing-playground pa-2">
+          <v-card-title>検索条件</v-card-title>
+          <v-divider> </v-divider>
+          <v-checkbox
+            v-model="isAllWardsSelected"
+            @click="selectAllWards"
+            hide-details
+            label="すべて"
+            class="mt-0"
+          ></v-checkbox>
+          <v-row>
+            <v-col
+              v-for="(wardpart, i) in [
+                wards.slice(0, 4),
+                wards.slice(4, 8),
+                wards.slice(8, 12),
+                wards.slice(12, 16),
+                wards.slice(16, 20),
+                wards.slice(20, 23),
+              ]"
+              v-bind:key="i"
+              cols="12"
+              sm="2"
+              md="2"
+            >
+              <v-checkbox
+                v-for="ward in wardpart"
+                v-bind:key="ward.id"
+                v-bind:label="ward.name"
+                :value="ward.id"
+                v-model="selectedWardIds"
+                hide-details
+                class="ward-checkbox mt-0"
+              ></v-checkbox>
+            </v-col>
+          </v-row>
+          <v-divider class="my-1"> </v-divider>
+          <v-checkbox
+            v-model="isAllFacilityTypesSelected"
+            @click="selectAllFacilityTypes"
+            hide-details
+            label="すべて"
+            class="mt-0"
+          ></v-checkbox>
+          <v-row>
+            <v-col
+              v-for="facilityType in facilityTypes"
+              v-bind:key="facilityType.id"
+              cols="12"
+              sm="2"
+              md="2"
+            >
+              <v-checkbox
+                v-bind:label="facilityType.name"
+                :value="facilityType.id"
+                v-model="selectedFacilityTypeIds"
+                hide-details
+                class="facility-type-checkbox mt-0"
+              ></v-checkbox>
+            </v-col>
+          </v-row>
+          <v-divider class="my-1"> </v-divider>
+          <v-row>
+            <v-col class="my-1">
+              <v-text-field
+                flat
+                solo-inverted
+                hide-details
+                prepend-inner-icon="mdi-magnify"
+                label="キーワード検索"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col class="pt-0"> <v-btn elevation="4" block>検索</v-btn></v-col>
+          </v-row>
         </v-card>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col
-        v-for="(wardpart, i) in [
-          wards.slice(0, 4),
-          wards.slice(4, 8),
-          wards.slice(8, 12),
-          wards.slice(12, 16),
-          wards.slice(16, 20),
-          wards.slice(20, 23),
-        ]"
-        v-bind:key="i"
-        cols="12"
-        sm="2"
-        md="2"
-      >
-        <v-checkbox
-          v-for="ward in wardpart"
-          v-bind:key="ward.value"
-          v-bind:label="ward.text"
-          v-bind:id="ward.key"
-          hide-details
-          value="true"
-        ></v-checkbox>
-      </v-col>
-    </v-row>
-
-    <v-row>
-      <v-col class="d-flex align-center" cols="12" sm="6">
-        <v-text-field>
-          <v-icon slot="append" color="red"> mdi-plus </v-icon>
-          <v-icon slot="prepend" color="green"> mdi-minus </v-icon>
-        </v-text-field>
-      </v-col>
-      <v-col class="d-flex align-center" cols="12" sm="6">
-        <v-select label="Outlined style" outlined></v-select>
       </v-col>
     </v-row>
   </v-container>
 </template>
-
+22
 <script>
 export default {
   data: () => ({
+    isAllWardsSelected: true,
+    isAllFacilityTypesSelected: true,
     wards: [
-      { value: 1, text: "足立区" },
-      { value: 2, text: "荒川区" },
-      { value: 3, text: "板橋区" },
-      { value: 4, text: "江戸川区" },
-      { value: 5, text: "大田区" },
-      { value: 6, text: "葛飾区" },
-      { value: 7, text: "北区" },
-      { value: 8, text: "江東区" },
-      { value: 9, text: "品川区" },
-      { value: 10, text: "渋谷区" },
-      { value: 11, text: "新宿区" },
-      { value: 12, text: "杉並区" },
-      { value: 13, text: "墨田区" },
-      { value: 14, text: "世田谷区" },
-      { value: 15, text: "台東区" },
-      { value: 16, text: "千代田区" },
-      { value: 17, text: "中央区" },
-      { value: 18, text: "豊島区" },
-      { value: 19, text: "中野区" },
-      { value: 20, text: "練馬区" },
-      { value: 21, text: "文京区" },
-      { value: 22, text: "港区" },
-      { value: 23, text: "目黒区" },
+      { id: 1, name: "足立区" },
+      { id: 2, name: "荒川区" },
+      { id: 3, name: "板橋区" },
+      { id: 4, name: "江戸川区" },
+      { id: 5, name: "大田区" },
+      { id: 6, name: "葛飾区" },
+      { id: 7, name: "北区" },
+      { id: 8, name: "江東区" },
+      { id: 9, name: "品川区" },
+      { id: 10, name: "渋谷区" },
+      { id: 11, name: "新宿区" },
+      { id: 12, name: "杉並区" },
+      { id: 13, name: "墨田区" },
+      { id: 14, name: "世田谷区" },
+      { id: 15, name: "台東区" },
+      { id: 16, name: "千代田区" },
+      { id: 17, name: "中央区" },
+      { id: 18, name: "豊島区" },
+      { id: 19, name: "中野区" },
+      { id: 20, name: "練馬区" },
+      { id: 21, name: "文京区" },
+      { id: 22, name: "港区" },
+      { id: 23, name: "目黒区" },
     ],
+    facilityTypes: [
+      { id: 1, name: "体育館" },
+      { id: 2, name: "卓球教室" },
+      { id: 3, name: "卓球バー" },
+      { id: 4, name: "卓球スタジオ" },
+      { id: 5, name: "その他" },
+    ],
+    selectedWardIds: [...Array(23)].map((_, i) => i + 1),
+    selectedFacilityTypeIds: [...Array(5)].map((_, i) => i + 1),
   }),
+  methods: {
+    selectAllWards() {
+      if (!this.isAllWardsSelected) {
+        this.selectedWardIds = [];
+        this.isAllWardsSelected = false;
+      } else {
+        this.selectedWardIds = [];
+        for (var ward in this.wards) {
+          this.selectedWardIds.push(this.wards[ward].id);
+        }
+        this.isAllWardsSelected = true;
+      }
+    },
+    selectAllFacilityTypes() {
+      if (!this.isAllFacilityTypesSelected) {
+        this.selectedFacilityTypeIds = [];
+        this.isAllFacilityTypesSelected = false;
+      } else {
+        this.selectedFacilityTypeIds = [];
+        for (var facilityType in this.facilityTypes) {
+          this.selectedFacilityTypeIds.push(
+            this.facilityTypes[facilityType].id
+          );
+        }
+        this.isAllFacilityTypesSelected = true;
+      }
+    },
+  },
 };
 </script>
-
-<style scoped>
-.v-input--selection-controls {
-  margin-top: 0;
-}
-</style>
