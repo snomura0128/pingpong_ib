b549b0deaf064a72f0d946d60b3e87ef69deb14d
snomura0128 <tomra.3104@gmail.com>
2021-03-01 23:27:16 +0900

    modify proxy setting

--- frontend/srv/index.js
+++ frontend/srv/index.js
@@ -58,6 +58,58 @@ export default (app, http) => {
         imageUrl: "http://ponzo.jp/img/photo_baba1.png",
         evaluation: 3.2,
       },
+      {
+        id: 7,
+        name: "新宿区コズミックセンター",
+        facilityTypes: [1],
+        imageUrl:
+          "https://www.regasu-shinjuku.or.jp/regasu/wp-content/uploads/2010/04/3e37f68e2bd1b9c7d00bd1aa7e5f7844-420x296.jpg",
+        evaluation: 4.2,
+        favorite: true,
+      },
+      {
+        id: 8,
+        name: "新宿スポーツセンター",
+        facilityTypes: [1],
+        imageUrl:
+          "http://www.shinjuku-sportscenter.com/images/facility/facility_main_b.jpg",
+        evaluation: 4.1,
+        favorite: true,
+      },
+      {
+        id: 9,
+        name: "江戸川区グリーンパレス",
+        facilityTypes: [1],
+        imageUrl:
+          "https://www.green-palace.jp/share/facility/img/facility_01.jpg",
+        evaluation: 4.1,
+        favorite: true,
+      },
+      {
+        id: 10,
+        name: "台東区リバーサイドスポーツセンター",
+        facilityTypes: [1, 2, 3, 5],
+        imageUrl:
+          "https://www.taitocity.net/zaidan/riverside/wp-content/uploads/sites/2/2015/12/river_top01.png",
+        evaluation: 5.0,
+        favorite: false,
+      },
+      {
+        id: 11,
+        name: "卓球空間FunTable",
+        facilityTypes: [2, 4],
+        imageUrl:
+          "https://funtable.info/wp-content/uploads/2020/12/junior-bosyu.jpg",
+        evaluation: 2.2,
+        favorite: false,
+      },
+      {
+        id: 12,
+        name: "卓球酒場 ぽん蔵 高田馬場店",
+        facilityTypes: [3],
+        imageUrl: "http://ponzo.jp/img/photo_baba1.png",
+        evaluation: 3.2,
+      },
     ];
     res.json(data);
   });
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -9,4 +9,9 @@ module.exports = {
       serverDir: './srv'
     }
   },
+  devServer: {
+    port: 8080,
+    // localhostでvueからexpressにAPIリクエストを送信する為の設定
+    proxy: 'http://localhost:3000'
+},
 }
